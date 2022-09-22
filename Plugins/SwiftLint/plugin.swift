1a20e9a9d0954d4f4389340a3e1a2cb332d6a931
Eric Jensen <ejensen@adobe.com>
2022-09-21 22:54:08 -0400

    Initial Commit

---
+++ Plugins/SwiftLint/plugin.swift
@@ -0,0 +1,36 @@
+import PackagePlugin
+
+@main
+struct SwiftLintPlugin: BuildToolPlugin {
+    func createBuildCommands(context: PackagePlugin.PluginContext, target: PackagePlugin.Target) async throws -> [PackagePlugin.Command] {
+        [
+            .buildCommand(
+                displayName: "SwiftLint \(target.name)",
+                executable: try context.tool(named: "swiftlint").path,
+                arguments: [
+                    "lint",
+                    "--cache-path", "\(context.pluginWorkDirectory)"
+                ]
+            )
+        ]
+    }
+}
+
+#if canImport(XcodeProjectPlugin)
+import XcodeProjectPlugin
+
+extension SwiftLintPlugin: XcodeBuildToolPlugin {
+    func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
+        [
+            .buildCommand(
+                displayName: "SwiftLint \(target.displayName)",
+                executable: try context.tool(named: "swiftlint").path,
+                arguments: [
+                    "lint",
+                    "--cache-path", "\(context.pluginWorkDirectory)"
+                ]
+            )
+        ]
+    }
+}
+#endif
---
+++ Plugins/SwiftLintFix/plugin.swift
@@ -0,0 +1,44 @@
+import PackagePlugin
+import Foundation
+
+@main
+struct SwiftLintFixPlugin: CommandPlugin {
+    func performCommand(context: PackagePlugin.PluginContext, arguments: [String]) async throws {
+        let executableURL = try URL(fileURLWithPath: context.tool(named: "swiftlint").path.string)
+        let process = Process()
+        process.executableURL = executableURL
+        process.arguments = ["--fix"]
+
+        try process.run()
+        process.waitUntilExit()
+
+        if process.terminationReason == .exit, process.terminationStatus == 0 {
+            Diagnostics.remark("SwiftLint corrected \(context.package.displayName)")
+        } else {
+            let problem = "\(process.terminationReason):\(process.terminationStatus)"
+            Diagnostics.error("swift-lint --fix \(context.package.displayName) invocation failed: \(problem)")
+        }
+    }
+}
+
+#if canImport(XcodeProjectPlugin)
+import XcodeProjectPlugin
+
+extension SwiftLintFixPlugin: XcodeCommandPlugin {
+    func performCommand(context: XcodePluginContext, arguments: [String]) throws {
+        let executableURL = try URL(fileURLWithPath: context.tool(named: "swiftlint").path.string)
+        let process = Process()
+        process.executableURL = executableURL
+        process.arguments = ["--fix"]
+        try process.run()
+        process.waitUntilExit()
+
+        if process.terminationReason == .exit, process.terminationStatus == 0 {
+            Diagnostics.remark("SwiftLint corrected \(context.xcodeProject.displayName)")
+        } else {
+            let problem = "\(process.terminationReason):\(process.terminationStatus)"
+            Diagnostics.error("swift-lint --fix \(context.xcodeProject.displayName) invocation failed: \(problem)")
+        }
+    }
+}
+#endif
